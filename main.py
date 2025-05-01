23cafa6bf8c669218774a89dfa0f9e11c3940c9a
stupidyoh <gattswet0926@gmail.com>
2025-05-01 15:03:59 +0900

    feat: endpoints 모듈 개발

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+# -*- coding: utf-8 -*-
+
 # 애플리케이션 초기화 및 구성 / 서버 시작점 역할 수행
 
 import os
 
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 
-from api.v1.endpoints.tuning_router import tuning_router
-from api.v1.endpoints.users_post_router import users_post_router
+from api.v1.endpoints.tuning_router import TuningRouter
+from api.v1.endpoints.users_post_router import UserPostRouter
+
+load_dotenv()
 
 # FastAPI 앱 인스턴스 생성
 app = FastAPI(
@@ -18,21 +23,15 @@ app = FastAPI(
 
 
 # 라우터 등록
-app.include_router(users_post_router, prefix="/api/v1/users", tags=["Users"])
-app.include_router(tuning_router, prefix="/api/v1/tuning", tags=["Tuning"])
-
-
-@app.get("/", tags=["Health"])
-async def health_check():
-    """API 헬스 체크 엔드포인트"""
-    return {"status": "healthy", "service": "TUNING API"}
+app.include_router(UserPostRouter().router)
+app.include_router(TuningRouter().router)
 
 
 # 개발 환경에서 Root Path 접속 시, swagger 페이지로 Redirect
-# @app.get("/")
-# async def root():
-#     env = os.getenv("ENVIRONMENT")
-#     if env == "dev":
-#         return RedirectResponse(url="/docs")
-#     else:
-#         return HTTPException(status_code=404)
+@app.get("/")
+async def root():
+    env = os.getenv("ENVIRONMENT")
+    if env == "dev":
+        return RedirectResponse(url="/docs")
+    else:
+        return HTTPException(status_code=404)
