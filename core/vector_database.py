30ccbc2b15ed903845839bc3e7ac39db650e29ef
Jinkyeong Kim <jinkyeongk@naver.com>
2025-05-01 20:06:14 +0900

    feat:ChromaDB  로드

--- core/vector_database.py
+++ core/vector_database.py
@@ -1,1 +1,9 @@
 # 벡터DB 관리(chromaDB 연동)
+
+import chromadb
+
+# ChromaDB 클라이언트 및 컬렉션 초기화
+chroma_client = chromadb.PersistentClient(path="./chroma_db")
+
+user_collection = chroma_client.get_or_create_collection("user_profiles")
+similarity_collection = chroma_client.get_or_create_collection("user_similarities")
