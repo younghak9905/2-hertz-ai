6c35f6d01ae5c1b763e9681e66de8e86ac7639c9
Jinkyeong Kim <jinkyeongk@naver.com>
2025-05-08 00:48:26 +0900

    chore: 데이터베이스 경로 및 requirements 버전 수정

--- app/core/vector_database.py
+++ app/core/vector_database.py
@@ -1,13 +1,15 @@
 # 벡터DB 관리(chromaDB 연동)
 
 # 로컬 ChromaDB 필요 임포트
-import os
+# import os
 
 import chromadb
 
+"""
 # 현재 파일 기준으로 루트 디렉토리 경로 계산
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 chroma_path = os.path.join(BASE_DIR, "chroma_db")
+
 # 로컬 ChromaDB 클라이언트 및 컬렉션 초기화
 chroma_client = chromadb.PersistentClient(path=chroma_path)
 """
@@ -15,13 +17,11 @@ chroma_client = chromadb.PersistentClient(path=chroma_path)
 # 서버 모드에서 ChromaDB에 연결
 chroma_client = chromadb.HttpClient(host="http://localhost:8001")
 
-"""
-
 user_collection = chroma_client.get_or_create_collection("user_profiles")
 similarity_collection = chroma_client.get_or_create_collection("user_similarities")
 
 
-async def get_user_data(user_id: str):
+def get_user_data(user_id: str):
     return user_collection.get(ids=[user_id], include=["metadatas"])
 
 
