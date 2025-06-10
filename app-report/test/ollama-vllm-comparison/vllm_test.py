cd6642dcd72e1f577db580b49832cdb7cbde6bab
stupidyoh <gattswet0926@gmail.com>
2025-06-10 11:28:30 +0900

    feat: Ollama/vLLM 단일/동시 요청에 대한 성능 비교 평가

---
+++ app-report/test/ollama-vllm-comparison/vllm_test.py
@@ -0,0 +1,83 @@
+import asyncio
+import time
+
+# import subprocess
+import GPUtil
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+
+
+def get_vram_usage():
+    gpus = GPUtil.getGPUs()
+    if gpus:
+        return gpus[0].memoryUsed  # MB 단위
+    return 0
+
+
+async def test_vllm():
+    # 모든 모델 언로드
+    # subprocess.run(["ollama", "stop", "qwen2.5:7b"], capture_output=True)
+    time.sleep(2)  # 언로드 대기
+
+    # 모델 초기화
+    inference_server_url = "http://localhost:8000/v1"
+    vllm_llm = ChatOpenAI(
+        model="Qwen/Qwen2.5-7B-Instruct-AWQ",
+        openai_api_key="EMPTY",
+        openai_api_base=inference_server_url,
+    )
+
+    # 간단한 에이전트 생성(도구 없음)
+    agent = create_react_agent(vllm_llm, [])
+
+    # # 테스트 메시지1
+    # messages = [
+    #     {"role": "user", "content": "55x55는?"},
+    # ]
+    # 테스트 메시지2
+    messages = [
+        {
+            "role": "system",
+            "content": "당신은 전문 기자입니다. 정확하고 객관적인 뉴스 기사를 작성해주세요. 답변은 json 형식으로 아래처럼 구성하세요:\n\n"
+            '{ "title": "뉴스 제목", "content": "뉴스 내용" }',
+        },
+        {
+            "role": "user",
+            "content": "AI 기술 발전에 대한 짧은 뉴스 기사를 한국어로 작성해주세요.",
+        },
+    ]
+
+    # 모델 실행 전 VRAM
+    vram_before = get_vram_usage()
+
+    # 모델 실행 및 시간 측정
+    start_time = time.time()
+    model_response = await agent.ainvoke({"messages": messages})
+    end_time = time.time()
+
+    print(f"\n모델 응답:\n{model_response}")
+
+    # 모델 실행 전 VRAM
+    vram_after = get_vram_usage()
+
+    # 모델 응답 확인
+    ai_message = model_response["messages"][-1]
+    print(f"\n모델 응답:\n{ai_message}")
+
+    # 응답시간 확인
+    response_time = end_time - start_time
+    print(f"\n응답시간: {response_time:.3f}초")
+
+    # 토큰 확인
+    token_usage = model_response["messages"][-1].usage_metadata
+    token_per_second = token_usage["output_tokens"] / response_time
+    print(f"\n토큰 수:\n{token_usage}")
+    print(f"초당 처리 토큰 수:\n{token_per_second}")
+
+    # VRAM 사용량 확인(vLLM 서버를 먼저 실행하기에 측정 불가)
+    vram_used = vram_after - vram_before
+    print(f"\nVRAM 사용량:\n {vram_used}MB")
+
+
+if __name__ == "__main__":
+    asyncio.run(test_vllm())
